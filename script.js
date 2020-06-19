c8f26e005f0d67a7fcd04fb071bbde4c1fa5afe4
Artistic-cat <sristi.sinha30@gmail.com>
2020-06-19 17:36:11 +0530

    Portfolio, removed slider, responsive

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   $('#autoWidth').lightSlider({
-       autoWidth:true,
-       loop:false,
-       onSliderLoad: function() {
-           $('autoWidth').removeClass('cS-hidden');
-       }
-   });
-});
